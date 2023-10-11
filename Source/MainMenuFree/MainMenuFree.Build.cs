560e45bae7be9ad8185d4b17e064dfdbe97dfbfa
ABurdukovskiy <bsaha@mail.ru>
2023-10-11 17:54:19 +0200

    + MainMenu Asset

--- Source/MainMenuFree/MainMenuFree.Build.cs
+++ Source/MainMenuFree/MainMenuFree.Build.cs
@@ -10,7 +10,7 @@ public class MainMenuFree : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "MainMenu_Free" });
 
 		// Uncomment if you are using Slate UI
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
